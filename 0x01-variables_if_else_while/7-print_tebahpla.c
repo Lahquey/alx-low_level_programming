d5e74b6702345920300c2852041fddec15b36aba
Lucky Griffin <luckygriffin2@gmail.com>
2023-07-17 09:17:49 -0700

    alx-low_level_programming/0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in reverse,
+ * follow by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
