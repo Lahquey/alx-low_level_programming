5c5c3fb2a660fe5600a440836e3c3a246c6032a9
Lucky Griffin <luckygriffin2@gmail.com>
2023-07-21 22:30:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print Alphabet - Print Alphabet in Lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
