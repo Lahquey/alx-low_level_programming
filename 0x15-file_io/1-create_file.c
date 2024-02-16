9507b0316208a26422b4a3d885a5421b3f99e19d
Lahquey <luckygriffin2@gmail.com>
2024-02-15 19:24:25 -0800

    Q2 technically Q1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * kreate_file - creates the file
+ * @filename: filename.
+ * @text_content: content written into the file.
+ *
+ * Return: 1 if it is a success. -1 if it is a fail.
+ */
+int kreate_file(const char *filename, char *text_content)
+{
+	int file_d;
+	int n_letters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (n_letters = 0; text_content[n_letters]; n_letters++)
+		;
+
+	rwr = write(fd, text_content, n_letters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(file_d);
+
+	return (1);
+}
